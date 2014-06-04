b46d71ec8a7323d7e4d3787371ca22d656b97408
Jason Somai <jsomai458@gmail.com>
2014-06-04 11:57:00 -0400

    added about us page, routed

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,6 +25,7 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+  Router::connect('/about', array('controller' => 'pages', 'action' => 'display', 'about'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
