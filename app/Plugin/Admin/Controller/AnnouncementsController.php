8d04e4dcb041aeabc4eea78780ac11926af0c043
Prateem Shrestha <prateems@gmail.com>
2014-06-06 00:16:34 -0400

    Added view logic

--- app/Plugin/Admin/Controller/AnnouncementsController.php
+++ app/Plugin/Admin/Controller/AnnouncementsController.php
@@ -6,8 +6,13 @@ class AnnouncementsController extends AdminAppController {
 
   }
 
-  public function view() {
-
+  public function view($slug) {
+    if ($data = $this->Announcement->findBySlug($slug)) {
+      $this->set('data', $data);
+    } else {
+      $this->Session->setFlash('Announcement not found.', 'error');
+      $this->redirect('index');
+    }
   }
 
   public function edit() {
@@ -19,7 +24,7 @@ class AnnouncementsController extends AdminAppController {
   }
 
   public function delete() {
-    
+
   }
 
   public function beforeRender() {
