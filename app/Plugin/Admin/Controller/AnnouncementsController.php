11275232a7ed7f18fed238baaaf6fe9b4912cc46
Prateem Shrestha <prateems@gmail.com>
2014-06-06 00:51:04 -0400

    Some controller logic

--- app/Plugin/Admin/Controller/AnnouncementsController.php
+++ app/Plugin/Admin/Controller/AnnouncementsController.php
@@ -3,7 +3,9 @@
 class AnnouncementsController extends AdminAppController {
   
   public function index() {
+    $data = $this->Announcement->find('all');
 
+    $this->set('data', $data);
   }
 
   public function view($slug) {
@@ -15,8 +17,17 @@ class AnnouncementsController extends AdminAppController {
     }
   }
 
-  public function edit() {
+  public function edit($id) {
+    if ($this->request->is('post')) {
 
+    }
+
+    if ($data = $this->Announcement->findById($id)) {
+      $this->set('data', $data);
+    } else {
+      $this->Session->setFlash('Announcement not found.', 'error');
+      $this->redirect('index');
+    }
   }
 
   public function add() {
